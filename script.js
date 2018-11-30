ddaaca5f53056aef8b068fe89f77f325b1c7013f
NateHiser <nate.hiser@gmail.com>
2018-11-30 17:38:41 -0400

    continue working

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var canvas = document.createElement('canvas');
-canvas.width = (window.innerWidth*0.9)*2; // I multiply the canvas size by 2 and scale it down with CSS to effectively create a @2x graphic for my retina display
+canvas.width = (window.innerWidth*0.9)*2;
 canvas.height = (window.innerHeight*0.9)*2;
 document.body.appendChild(canvas);
 
@@ -14,7 +14,10 @@ if (!gl) {
 	alert('Your browser does not support WebGl');
 }
 
-var vertexShader = gl.createShader(gl.VERTEX_SHADER);
+gl.enable(gl.DEPTH_TEST);
+gl.enable(gl.CULL_FACE);
+gl.frontFace(gl.CCW);
+gl.cullFace(gl.BACK);
 
 var vertexShader = gl.createShader(gl.VERTEX_SHADER);
 gl.shaderSource(vertexShader, `
@@ -77,13 +80,6 @@ gl.validateProgram(program);
 if (!gl.getProgramParameter(program, gl.VALIDATE_STATUS)) {
 	console.error('ERROR validating program', gl.getProgramInfolog(program));
 }
-
-gl.useProgram(program);`.
-
-var animate = function() {
-		requestAnimationFrame(animate);
-	}
-	animate();
 
   var boxVertices = [
 	-1.00,  1.00, -1.00,	 0.00,  0.00,
@@ -171,3 +167,50 @@ gl.vertexAttribPointer(
   3 * Float32Array.BYTES_PER_ELEMENT
 );
 gl.enableVertexAttribArray(texCoordAttribLocation);
+
+var boxTexture = gl.createTexture();
+gl.bindTexture(gl.TEXTURE_2D, boxTexture);
+gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, 1, 1, 0, gl.RGB, gl.UNSIGNED_BYTE,
+              new Uint8Array([255, 255, 255]));
+
+gl.useProgram(program);
+var matWorldUniformLocation = gl.getUniformLocation(program, 'mWorld');
+var matViewUniformLocation = gl.getUniformLocation(program, 'mView');
+var matProjUniformLocation = gl.getUniformLocation(program, 'mProj');
+
+var worldMatrix = mat4.create();
+var viewMatrix = mat4.create();
+var projMatrix = mat4.create();
+
+
+mat4.identity(worldMatrix);
+mat4.lookAt(viewMatrix, [0, 0, 6], [0, 0, 0], [0, 1, 0]);
+mat4.perspective(projMatrix, glMatrix.toRadian(45), canvas.width/canvas.height, 0.1, 1000.0);
+
+gl.uniformMatrix4fv(matProjUniformLocation, gl.FALSE, projMatrix);
+gl.uniformMatrix4fv(matViewUniformLocation, gl.FALSE, viewMatrix);
+
+var xRotationMatrix = mat4.create();
+var yRotationMatrix = mat4.create();
+var identityMatrix = mat4.create();
+mat4.identity(identityMatrix);
+
+var animate = function() {
+
+	angle = performance.now() / 1000 / 6 * 2 * Math.PI;
+
+	// Applies the transformations
+	mat4.rotateY(yRotationMatrix, identityMatrix, angle);
+	mat4.rotateX(xRotationMatrix, identityMatrix, angle / 4);
+	mat4.mul(worldMatrix, yRotationMatrix, xRotationMatrix);
+
+	// Sends the updated matrix to the GPU
+	gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, worldMatrix);
+
+	// Actually performs the draw operation
+	gl.drawElements(gl.TRIANGLES, boxIndices.length, gl.UNSIGNED_SHORT, 0);
+
+	requestAnimationFrame(animate);
+}
+animate();
+}());
